e60943e05778cf93682e6d990911e0a4d728690b
shruti0511 <patelshrutiv@gmail.com>
2025-06-13 12:13:51 +0530

    Implemented newline support. i. single newline. ii. multi newline
    Test 1.  new line between 2 numbers should implement add.
    Test 2. multi new lines between numbers should implement add.

--- src/stringcalculator.ts
+++ src/stringcalculator.ts
@@ -1,10 +1,14 @@
 export class StringCalculator {
     add(numbers: string): number {
+        const DEFAULT_SEPARATOR = ',';
+
         if (!numbers) {
             return 0
         }
-        
-        const arr = numbers.split(',').map(num => parseInt(num.trim(), 10));
+
+        const normalizedNumber = numbers.replace(/\n/g, DEFAULT_SEPARATOR);
+        const arr = normalizedNumber.split(DEFAULT_SEPARATOR).map(num => parseInt(num.trim(), 10));
         return arr.reduce((acc, val) => acc + val, 0)
     }
+
 }
--- test/stringcalculator.test.ts
+++ test/stringcalculator.test.ts
@@ -21,4 +21,13 @@ describe('Calculator', () => {
         });
     });
 
+    describe('Newline Separator', () => {
+        test('Single Newline: should evaluate "5\n4" to 9', () => {
+            expect(calculator.add("5\n4")).toBe(9)
+        });
+        test('Multi Newline: should evaluate "5\n4\n11\n2" to 22', () => {
+            expect(calculator.add('5\n4\n11\n2')).toBe(22)
+        });
+    });
+
 })
